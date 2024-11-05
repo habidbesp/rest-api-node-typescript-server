abce0c7c24ddc6532bf39a5592ffe173968e9d00
Habid B Espinosa <hab.bad@hotmail.com>
2024-11-05 21:26:15 +0100

    testing with jest and supertest

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -2,111 +2,66 @@ import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
 export const getProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await Product.findAll({
-      // order: [["price", "DESC"]],
-      // attributes: { exclude: ["createdAt", "updatedAt", "availability"] },
-    });
-    res.json({ data: products });
-  } catch (error) {
-    console.log(error);
-  }
+  const products = await Product.findAll({
+    order: [["price", "DESC"]],
+  });
+  res.json({ data: products });
 };
 
 export const getProductsById = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByPk(id);
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
 
-    if (!product) {
-      res.status(404).json({ error: "Product not Found" });
-      return;
-    }
-
-    res.json({ data: product });
-  } catch (error) {
-    console.log(error);
+  if (!product) {
+    res.status(404).json({ error: "Product not Found" });
+    return;
   }
+
+  res.json({ data: product });
 };
 
 export const createProduct = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.create(req.body);
-    res.json({ data: product });
-  } catch (error) {
-    console.log(error);
-  }
+  const product = await Product.create(req.body);
+  res.status(201).json({ data: product });
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByPk(id);
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
 
-    if (!product) {
-      res.status(404).json({ error: "Product not Found" });
-      return;
-    }
-
-    const updatedProduct = await product.update(req.body);
-
-    if (!updatedProduct) {
-      res
-        .status(500)
-        .json({ error: "Internal Server Error: The product was not updated." });
-      return;
-    }
-
-    res.json({ data: updatedProduct });
-  } catch (error) {
-    console.error("Error updating product:", error);
-    res
-      .status(500)
-      .json({ error: "Internal Server Error: Something went wrong." });
+  if (!product) {
+    res.status(404).json({ error: "Product not Found" });
     return;
   }
+
+  const updatedProduct = await product.update(req.body);
+
+  res.json({ data: updatedProduct });
 };
 
 export const updateAvailability = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByPk(id);
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
 
-    if (!product) {
-      res.status(404).json({ error: "Product not Found" });
-      return;
-    }
-
-    product.availability = !product.dataValues.availability;
-    await product.save();
-
-    res.json({ data: product });
-  } catch (error) {
-    console.error("Error updating availability:", error);
-    res
-      .status(500)
-      .json({ error: "Internal Server Error: Something went wrong." });
+  if (!product) {
+    res.status(404).json({ error: "Product not Found" });
     return;
   }
+
+  product.availability = !product.dataValues.availability;
+  await product.save();
+  res.json({ data: product });
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByPk(id);
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
 
-    if (!product) {
-      res.status(404).json({ error: "Product not Found" });
-      return;
-    }
-
-    await product.destroy();
-    res.json({ data: "Product deleted." });
-  } catch (error) {
-    console.error("Error deleting product:", error);
-    res
-      .status(500)
-      .json({ error: "Internal Server Error: Something went wrong." });
+  if (!product) {
+    res.status(404).json({ error: "Product not Found" });
     return;
   }
+
+  await product.destroy();
+  res.json({ data: "Product deleted" });
 };
